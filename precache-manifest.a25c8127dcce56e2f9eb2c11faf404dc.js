1c3d8c496f1f09bc9e56dc9c12e7226772562c49
LakshmanaBP <64594069+LakshmanaBP@users.noreply.github.com>
2020-08-04 09:06:05 +0530

    Updates

---
+++ precache-manifest.a25c8127dcce56e2f9eb2c11faf404dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a06dac59ccc99a1008dbf0d8c4ac8d",
+    "url": "/todoApplication/index.html"
+  },
+  {
+    "revision": "92a417a877afa8ff354e",
+    "url": "/todoApplication/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "895d31552c51ba2eccbe",
+    "url": "/todoApplication/static/js/2.53d0a898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApplication/static/js/2.53d0a898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a417a877afa8ff354e",
+    "url": "/todoApplication/static/js/main.252cce03.chunk.js"
+  },
+  {
+    "revision": "ca2bb15a6474cd44b98d",
+    "url": "/todoApplication/static/js/runtime-main.0612ea0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoApplication/static/media/logo.5d5d9eef.svg"
+  }
+]);
